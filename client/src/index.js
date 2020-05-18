c039eb1b14f3ff75f28c0f2ddf6133b73dd1c450
Damien <damien-git@users.noreply.github.com>
2020-05-18 15:09:51 -0400

    no longer using oaf-bootstrap-4 (which was not useful the way it was used), using bootstrap-scss instead of bootstrap to avoid a jquery optional dependency

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Bootstrap (we only need the SCSS, there is no need for any Javascript
+// with react-bootstrap).
+import "bootstrap-scss/bootstrap.scss";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-// Accessibility fixes that need to come _before_ the Bootstrap import.
-import "oaf-bootstrap-4/scss/top.scss";
-
-// Bootstrap itself.
-import "bootstrap/scss/bootstrap.scss";
-
-// Accessibility fixes that need to come _after_ the Bootstrap import.
-import "oaf-bootstrap-4/scss/bottom.scss";
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
